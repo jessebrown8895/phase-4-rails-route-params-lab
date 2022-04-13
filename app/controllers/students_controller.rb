2d1b984151de83ed24c493caa9754b9239ae16a9
Jesse Brown <Brownjesse8895@yahoo.com>
2022-04-13 15:52:27 -0500

    commit tree 975c04a3f5c0d3bf8a6edad7ae8bd4b6e9569aaf

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    students = if Student.find_by(params[:name])
+    return render json: students
+    end
   end
+  
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end 
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,9 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  # def self.find_student_by_id
+
+  #   Student.find(params[:id])
+  # end
+
 end
